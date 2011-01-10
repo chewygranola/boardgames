d8cbc9dff37faae0573d1294adc4ecd6e02761e1
midnightslice <ijilagan@chewygranola.net>
2011-01-10 10:25:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boardgames::Application.config.secret_token = 'bcea81a676de5d3448fecb31c68cb732cdb1a85233e25f57ed55ed8228e28273d19814551fa6a063a7733da8f0156483baddc98bb75e3ffbcd1c10965eec4ee7'
